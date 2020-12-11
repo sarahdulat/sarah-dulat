a532ed59f09c879d9ee192f00ceb8c68aa6ffb95
Sarah Dulat <sarah.dulat@gmail.com>
2020-12-10 23:48:53 -0500

    adding social header

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
+    <Header />
     <Hero />
   </div>
 </template>
 
 <script>
+import Header from './components/Header.vue'
 import Hero from './components/Hero.vue'
 
 export default {
   name: 'App',
   components: {
+    Header,
     Hero
   }
 }
---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+	<div
+		id="footer"
+		class="sherbert">
+	  	<p>
+	  		<a href="https://github.com/sarahdulat" target="_blank">GitHub</a> | <a href="https://www.linkedin.com/in/sarahdulat/" target="_blank">LinkedIn</a> | <a href="https://twitter.com/sarahdulat" target="_blank">Twitter</a> | <a href="https://instagram.com/sarahdulat" target="_blank">Instagram</a>
+	  	</p>
+	</div>
+</template>
