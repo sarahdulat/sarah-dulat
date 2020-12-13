fb059cde7027a5be435e299c0fd5687ee017f165
Sarah Dulat <sarah.dulat@gmail.com>
2020-12-13 18:28:59 -0500

    ading favicon

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,6 +3,7 @@
         id="footer"
         class="sherbert">
         <p>
+			<span>© {{ currentYear }} Sarah Dulat</span>
             <a href="https://github.com/sarahdulat" target="_blank">GitHub</a> | <a href="https://www.linkedin.com/in/sarahdulat/" target="_blank">LinkedIn</a> | <a href="https://twitter.com/sarahdulat" target="_blank">Twitter</a>
         </p>
     </div>
@@ -10,6 +11,11 @@
 
 <script>
 export default {
-  name: 'Footer'
+    name: 'Footer',
+    data () {
+        return {
+            currentYear: new Date().getFullYear()
+        }
+    }
 }
 </script>
