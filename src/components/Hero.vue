e55c038cc8dd3f91ca1b34fdf90b1ea24e596270
Sarah Dulat <sarah.dulat@gmail.com>
2020-12-13 18:48:56 -0500

    adding animated class

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -2,7 +2,7 @@
   <div id="hero" class="sherbert">
     <div class="container">
         <h2 class="animated fadeIn">Started with GeoCities now I'm here.</h2>
-        <h1 class="animated fadeIn delay1">Hi there, my name is <strong>Sarah Dulat</strong> and I am a front end developer at <a href="http://www.reorg-research.com" target="_blank">Reorg</a> in NYC (<a href="https://jobs.lever.co/reorgresearch?lever-via=gozPBhAQBk" target="_blank">we're hiring!</a>).</h1>
+        <h1 class="animated fadeIn delay1">Hi there, my name is Sarah Dulat and I am a front end developer at <a href="http://www.reorg-research.com" target="_blank">Reorg</a> in NYC (<a href="https://jobs.lever.co/reorgresearch?lever-via=gozPBhAQBk" target="_blank">we're hiring!</a>).</h1>
         <h2 class="animated fadeIn delay2">In my former life I was a graphic designer and have now merged that experience with this new branch in my career. When I'm not fixing bugs I'm probably at hot yoga, ballet or learning something new.</h2>
     </div>
   </div>
