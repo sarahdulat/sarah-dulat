1149b3f3f59bde690b8361e2250acc32699589d0
Sarah Dulat <sarah.dulat@gmail.com>
2020-12-13 19:35:50 -0500

    adding contact content

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -2,6 +2,17 @@
   <div id="contact">
     <div class="container">
 		<h1>Contact</h1>
+		<div class="row">
+            <div class="column">
+                PHOTO
+            </div>
+            <div class="column">
+                <h3>Thanks for stopping by.</h3>
+                <p>CONTACT</p>
+                <p>(847) 877-1347</p>
+                <p><a href="mailto: sarah@sarahdulat.com">sarah@sarahdulat.com</a></p>
+            </div>
+        </div>
     </div>
   </div>
 </template>
--- src/components/Work.vue
+++ src/components/Work.vue
@@ -2,6 +2,17 @@
   <div id="work">
     <div class="container">
 		<h1>Work</h1>
+		<div class="row">
+            <div class="column">
+                WORK
+            </div>
+            <div class="column">
+                WORK
+            </div>
+            <div class="column">
+               WORK 
+            </div>
+        </div>
     </div>
   </div>
 </template>
