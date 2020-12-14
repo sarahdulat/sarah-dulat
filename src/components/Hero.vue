f832710d4615ca7efffc7207a2c3feff32ff871d
Sarah Dulat <sarah.dulat@gmail.com>
2020-12-13 23:30:38 -0500

    styling hero for mobile

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,9 +1,16 @@
 <template>
   <div id="hero" class="sherbert">
     <div class="container">
-        <h2 class="animated fadeIn">Started with GeoCities now I'm here.</h2>
-        <h1 class="animated fadeIn delay1">Hi there, my name is<br /><span class="name">Sarah Dulat</span>.</h1>
-        <h2 class="animated fadeIn delay2">I am a front end developer at <a href="http://www.reorg-research.com" target="_blank">Reorg</a> in NYC (<a href="https://jobs.lever.co/reorgresearch?lever-via=gozPBhAQBk" target="_blank">we're hiring!</a>). Former graphic designer turned web developer who loves good UI/UX, clean architecture and Vue.</h2>
+        <h2 class="animated fadeIn">
+            Started with GeoCities now I'm here.
+        </h2>
+        <h1 class="animated fadeIn delay1 no-wrap">
+            Hi there, my name is<br />
+            <span class="name">Sarah Dulat</span>.
+        </h1>
+        <h2 class="animated fadeIn delay2">
+            I am a front end developer at <a href="http://www.reorg-research.com" target="_blank">Reorg</a> in NYC (<a href="https://jobs.lever.co/reorgresearch?lever-via=gozPBhAQBk" target="_blank">we're hiring!</a>). Former graphic designer turned web developer who loves good <strong>UI/UX</strong>, clean architecture and <strong>VueJS</strong>.
+        </h2>
     </div>
   </div>
 </template>
