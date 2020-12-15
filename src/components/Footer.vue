508edbe704fcd38ff3a7d5a18a215c3e3550b21a
Sarah Dulat <sarah.dulat@gmail.com>
2020-12-14 22:11:39 -0500

    Styling footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,10 +2,12 @@
     <div
         id="footer"
         class="sherbert">
-        <p>
+        <div class="row">
 			<span>© {{ currentYear }} Sarah Dulat</span>
-            <a href="https://github.com/sarahdulat" target="_blank">GitHub</a> | <a href="https://www.linkedin.com/in/sarahdulat/" target="_blank">LinkedIn</a> | <a href="https://twitter.com/sarahdulat" target="_blank">Twitter</a>
-        </p>
+            <div class="social">
+                <a href="https://github.com/sarahdulat" target="_blank">GitHub</a> | <a href="https://www.linkedin.com/in/sarahdulat/" target="_blank">LinkedIn</a> | <a href="https://twitter.com/sarahdulat" target="_blank">Twitter</a>
+            </div>
+        </div>
     </div>
 </template>
 
