ad4a07393a8eef0e8b65c4c04879fa43a283d623
Sarah Dulat <sarah.dulat@gmail.com>
2020-12-17 23:00:21 -0500

    styling contact section

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,16 +1,15 @@
 <template>
   <div id="contact">
     <div class="container">
-		<h1>Contact</h1>
-		<div class="row">
+        <div class="row">
             <div class="column">
-                PHOTO
+                <div class="photo"></div>
             </div>
             <div class="column">
-                <h3>Thanks for stopping by.</h3>
-                <p>How to reach me:</p>
-                <p>(847) 877-1347</p>
-                <p><a href="mailto: sarah@sarahdulat.com">sarah@sarahdulat.com</a></p>
+                <h2>Thanks for stopping by.</h2>
+                <p>
+                    Keep in <a href="mailto: sarah@sarahdulat.com">touch</a>, find me on <a href="#">LinkedIn</a> or take a look at my <a href="#">resume<i class="fas fa-file-pdf"></i></a>.
+                </p>
             </div>
         </div>
     </div>
