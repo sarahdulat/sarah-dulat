11b4eb59164f6ed0547a5a58c46a1bee92344f48
Sarah Dulat <sarah.dulat@gmail.com>
2020-12-19 00:01:50 -0500

    styling skills section

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,25 +1,25 @@
 <template>
   <div id="skills">
     <div class="container">
-        <div class="row skills">
+        <div class="row">
             <div class="column">
-                <div><i class="fab fa-2x fa-vuejs"></i> VueJS</div>
-                <div><i class="fab fa-2x fa-js"></i> JavaScript</div>
-                <div><i class="fab fa-2x fa-react"></i> React</div>
-                <div><i class="fab fa-2x fa-sass"></i> Sass</div>
-                <div><i class="fab fa-2x fa-bootstrap"></i> Bootstrap</div>
+                <div class="skill"><i class="fab fa-2x fa-vuejs"></i> VueJS</div>
+                <div class="skill"><i class="fab fa-2x fa-js"></i> JavaScript</div>
+                <div class="skill"><i class="fab fa-2x fa-react"></i> React</div>
+                <div class="skill"><i class="fab fa-2x fa-sass"></i> Sass</div>
+                <div class="skill"><i class="fab fa-2x fa-bootstrap"></i> Bootstrap</div>
             </div>
             <div class="column">
-                <div><i class="fas fa-2x fa-database"></i> SQL</div>
-                <div><i class="fab fa-2x fa-github"></i> GitHub</div>
-                <div><i class="fab fa-2x fa-git-alt"></i> Git</div>
-                <div><i class="fab fa-2x fa-git-alt"></i> D3.js</div>
+                <div class="skill"><i class="fas fa-2x fa-database"></i> SQL</div>
+                <div class="skill"><i class="fab fa-2x fa-github"></i> GitHub</div>
+                <div class="skill"><i class="fab fa-2x fa-git-alt"></i> Git</div>
+                <div class="skill"><i class="fab fa-2x fa-git-alt"></i> D3.js</div>
             </div>
             <div class="column">
-                <div><i class="fas fa-2x fa-database"></i> RESTful APIs</div>
-                <div><i class="fab fa-2x fa-github"></i> InDesign</div>
-                <div><i class="fab fa-2x fa-git-alt"></i> Photoshop</div>
-                <div><i class="fab fa-2x fa-git-alt"></i> Illustrator</div>
+                <div class="skill"><i class="fas fa-2x fa-database"></i> RESTful APIs</div>
+                <div class="skill"><i class="fab fa-2x fa-github"></i> InDesign</div>
+                <div class="skill"><i class="fab fa-2x fa-git-alt"></i> Photoshop</div>
+                <div class="skill"><i class="fab fa-2x fa-git-alt"></i> Illustrator</div>
             </div>
         </div>
     </div>
