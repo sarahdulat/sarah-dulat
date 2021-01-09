b89989efeb455ed5c10437d299d54b948fd53838
Sarah Dulat <sarah.dulat@gmail.com>
2021-01-08 23:28:52 -0500

    adding linked in link

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -11,7 +11,7 @@
             <div class="column">
                 <h2>Thanks for stopping by.</h2>
                 <p>
-                    Keep in <a href="mailto: sarah@sarahdulat.com">touch</a>, find me on <a href="#">LinkedIn</a> or take a look at my <a href="#">resume<i class="fas fa-file-pdf"></i></a>.
+                    Keep in <a href="mailto: sarah@sarahdulat.com">touch</a>, find me on <a href="https://www.linkedin.com/in/sarahdulat/">LinkedIn</a> or take a look at my <a href="#">resume<i class="fas fa-file-pdf"></i></a>.
                 </p>
             </div>
         </div>
