e3b1acb7bd83ba52ec157a09d2e6e7fe461b777f
Sarah Dulat <sarah.dulat@gmail.com>
2021-01-16 20:29:12 -0500

    adding missing kill icons

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -13,7 +13,7 @@
             <div class="column">
                 <div class="skill"><i class="fab fa-2x fa-sass"></i> Sass</div>
                 <div class="skill"><i class="fab fa-2x fa-bootstrap"></i> Bootstrap</div>
-                <div class="skill"><i class="fab fa-2x fa-git-alt"></i> D3.js</div>
+                <div class="skill"><img src="http://www.sarahdulat.com/assets/d3.png" height="37" width="37" /> D3.js</div>
             </div>
             <div class="column">
                 <div class="skill"><i class="fas fa-2x fa-database"></i> MySQL</div>
@@ -21,9 +21,9 @@
                 <div class="skill"><i class="fab fa-2x fa-git-alt"></i> Git</div>
             </div>
             <div class="column" alt="Icons made by Freepik from www.flaticon.com">
-                <div class="skill"><i class="fab fa-2x fa-github"></i> InDesign</div>
-                <div class="skill"><i class="fab fa-2x fa-git-alt"></i> Photoshop</div>
-                <div class="skill"><i class="fab fa-2x fa-git-alt"></i> Illustrator</div>
+                <div class="skill"><i class="fi adobe-indesign"></i> InDesign</div>
+                <div class="skill"><i class="fi adobe-photoshop"></i> Photoshop</div>
+                <div class="skill"><i class="fi adobe-illustrator"></i> Illustrator</div>
             </div>
         </div>
     </div>
