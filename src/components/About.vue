d55d517ebdb2ccc9f2cb04f86f546fbb1617c156
Sarah Dulat <sarah.dulat@gmail.com>
2021-02-16 23:19:55 -0500

    editing bio

--- src/components/About.vue
+++ src/components/About.vue
@@ -24,7 +24,7 @@
                         Being a product-minded developer I am curious about seeking to understand the project in order to see all the options and edge cases before writing any code. From mock ups to architecture I like organized and consistent structure that won't make a user second guess what to do next. I find there are always ways to speed things up and do regular refactors as I learn and grow.
                     </p>
                     <p>
-                        In my free time I stay active with yoga and ballet, learn new things (most recently Portuguese), optimize my skincare routine and connect with friends by way of Among Us. <img class="among-us" src="http://www.sarahdulat.com/assets/among-us.png" alt="My Among Us avatar" />
+                        In my free time I stay active with yoga and ballet, learn new things (most recently Portuguese), yearn to travel again and connect with friends by way of Among Us. <img class="among-us" src="http://www.sarahdulat.com/assets/among-us.png" alt="My Among Us avatar" />
                     </p>
                 </div>
             </div>
