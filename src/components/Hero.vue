76717c18e38569859adbe9dbb4cf8d82171eaa34
Sarah Dulat <sarah.dulat@gmail.com>
2022-06-28 00:15:09 +0200

    publishing on github pages

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="hero" class="sherbert">
     <div class="container">
-        <h2 class="animated hero-fade">
-            Started with GeoCities now I'm here.
-        </h2>
-        <h1 class="animated hero-fade delay1 no-wrap">
-            Hi there, my name is<br />
-            <span class="name">Sarah Dulat</span>.
-        </h1>
-        <h2 class="animated hero-fade delay2">
-            I am a <strong>product-minded front end developer</strong> at <a href="http://www.reorg.com" target="_blank">Reorg</a> in NYC. Former designer turned developer who loves good UI/UX, clean architecture and VueJS.
-        </h2>
+      <h2 class="animated hero-fade">
+        Started with GeoCities now I'm here.
+      </h2>
+      <h1 class="animated hero-fade delay1 no-wrap">
+        Hi there, my name is<br />
+        <span class="name">Sarah Dulat</span>.
+      </h1>
+      <h2 class="animated hero-fade delay2">
+        I am a <strong>product-minded frontend engineer</strong> at <a href="http://www.brightfunds.org" target="_blank">Bright Funds</a>. Former designer turned developer who loves good UI/UX, clean architecture and VueJS.
+      </h2>
     </div>
   </div>
 </template>
